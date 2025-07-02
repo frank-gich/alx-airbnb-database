24ca31cf73439be6253dd3d7945c3d27d2ac81ba
Collin-5 <collinsotieno380@gmail.com>
2025-07-02 16:22:20 +0300

    databaes Aggregation & Window Functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -33,3 +33,24 @@ FROM (
     GROUP BY 
         p.property_id
 ) AS property_stats;
+
+
+-- 2. Total number of bookings made by each user
+SELECT 
+    b.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+GROUP BY b.user_id, u.first_name, u.last_name;
+
+-- 2. Rank properties by total number of bookings using ROW_NUMBER()
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name;
